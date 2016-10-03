667d7e327859408862784ff44dcc41b98a84922e
Shozomon <victor_shozo@hotmail.com>
2016-10-02 21:47:55 -0300

    started siloAdd

--- app/src/main/java/usjt/graincare/models/BeaconHistory.java
+++ app/src/main/java/usjt/graincare/models/BeaconHistory.java
@@ -1,5 +1,63 @@
 package usjt.graincare.models;
 
 public class BeaconHistory {
-    
+    private Long id;
+    private Beacon beacon;
+    private SiloHistory siloHistory;
+    private Double temperature;
+    private Double distance;
+    private Double humidity;
+
+    public BeaconHistory(){
+    }
+
+    public BeaconHistory(Long id, Beacon beacon, SiloHistory siloHistory, Double temperature, Double distance, Double humidity) {
+        this.id = id;
+        this.beacon = beacon;
+        this.siloHistory = siloHistory;
+        this.temperature = temperature;
+        this.distance = distance;
+        this.humidity = humidity;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Beacon getBeacon() {
+        return beacon;
+    }
+
+    public SiloHistory getSiloHistory() {
+        return siloHistory;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
 }
--- app/src/main/java/usjt/graincare/models/SiloHistory.java
+++ app/src/main/java/usjt/graincare/models/SiloHistory.java
@@ -1,4 +1,69 @@
 package usjt.graincare.models;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class SiloHistory {
+    private Long id;
+    private Grao grao;
+    private List<BeaconHistory> beacons;
+    private Calendar openedAt;
+    private Calendar closedAt;
+    private Boolean open;
+    private Silo silo;
+
+    public SiloHistory(Long id, Grao grao, List<BeaconHistory> beacons, Calendar openedAt, Calendar closedAt, Boolean open, Silo silo){
+
+        this.id = id;
+        this.grao = grao;
+        this.beacons = beacons;
+        this.openedAt = openedAt;
+        this.closedAt = closedAt;
+        this.open = open;
+        this.silo = silo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Grao getGrao() {
+        return grao;
+    }
+
+    public List<BeaconHistory> getBeacons() {
+        return beacons;
+    }
+
+    public Calendar getOpenedAt() {
+        return openedAt;
+    }
+
+    public void setOpenedAt(Calendar openedAt) {
+        this.openedAt = openedAt;
+    }
+
+    public Calendar getClosedAt() {
+        return closedAt;
+    }
+
+    public void setClosedAt(Calendar closedAt) {
+        this.closedAt = closedAt;
+    }
+
+    public Boolean getOpen() {
+        return open;
+    }
+
+    public void setOpen(Boolean open) {
+        this.open = open;
+    }
+
+    public Silo getSilo() {
+        return silo;
+    }
 }
